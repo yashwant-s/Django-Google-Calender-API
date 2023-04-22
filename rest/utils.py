9c7d285f332817d713c9c8c87bdcaa09fee6f296
yashwant-s <72819773+yashwant-s@users.noreply.github.com>
2023-04-22 10:11:32 +0000

    added comments

--- rest/utils.py
+++ rest/utils.py
@@ -3,24 +3,49 @@ from django.conf import settings
 from googleapiclient.discovery import build
 
 class GoogleCalenderUtils:
+    """
+    Utility class for Google Calendar API operations.
+    """
     @staticmethod
     def get_oauth2_flow(scopes, redirect_uri):
+        """
+        Creates an OAuth2 flow for Google Calendar API.
+
+        Args:
+            scopes (list): List of scopes to request access to.
+            redirect_uri (str): Redirect URI for the OAuth2 flow.
+
+        Returns:
+            flow (google_auth_oauthlib.flow.Flow): The OAuth2 flow.
+
+        """
         flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-            settings.CLIENT_SECRET_FILE,
-            scopes=scopes
+            settings.CLIENT_SECRET_FILE,  # Load client secrets file
+            scopes=scopes  # Set the scopes for Google Calendar API
         )
 
-        flow.redirect_uri = redirect_uri
-        return flow
+        flow.redirect_uri = redirect_uri  # Set the redirect URI for the OAuth2 flow
+        return flow  # Return the OAuth2 flow
 
     @staticmethod
     def get_event_list(credentials):
-        service = build('calendar', 'v3', credentials=credentials)
-        calendar_list = service.calendarList().list().execute()
-        event_list = []
+        """
+        Retrieves a list of events from Google Calendar API.
+
+        Args:
+            credentials (google.oauth2.credentials.Credentials): User credentials for accessing Google Calendar API.
+
+        Returns:
+            event_list (list): List of events retrieved from the Google Calendar API.
+
+        """
+        service = build('calendar', 'v3', credentials=credentials)  # Build the Calendar API service
+        calendar_list = service.calendarList().list().execute()  # Get the list of calendars
+        event_list = []  # Initialize the list to store events
 
         for calendar in calendar_list.get('items', []):
             calendar_id = calendar['id']
-            events = service.events().list(calendarId=calendar_id).execute()
-            event_list.extend(events.get('items', []))
-        return event_list
+            events = service.events().list(calendarId=calendar_id).execute()  # Get the events for each calendar
+            event_list.extend(events.get('items', []))  # Extend the event list with the retrieved events
+
+        return event_list  # Return the list of events
--- rest/views.py
+++ rest/views.py
@@ -7,45 +7,73 @@ from .utils import GoogleCalenderUtils
 
 
 class GoogleCalendarInitView(APIView):
+    """
+    Initiates the Google Calendar authorization flow.
 
+    This view initiates the OAuth2 authorization flow for Google Calendar API.
+    The user is redirected to the Google authorization page to grant permissions.
+    After successful authorization, the user is redirected to the redirect URI.
+
+    """
     def get(self, request):
-        scopes = GOOGLE_CALENDAR_SCOPES
-        redirect_uri = GOOGLE_CALENDAR_REDIRECT_URI
-        flow = GoogleCalenderUtils.get_oauth2_flow(scopes, redirect_uri)
+        """
+        Handles the GET request for initiating the Google Calendar authorization flow.
 
+        Returns:
+            redirect: Redirects the user to the Google authorization page.
+
+        """
+        scopes = GOOGLE_CALENDAR_SCOPES  # Set the scopes for Google Calendar API
+        redirect_uri = GOOGLE_CALENDAR_REDIRECT_URI  # Set the redirect URI for OAuth2 flow
+        flow = GoogleCalenderUtils.get_oauth2_flow(scopes, redirect_uri)  # Create an OAuth2 flow
+
+        # Get the authorization URL and state for offline access with granted scopes
         authorization_url, state = flow.authorization_url(
             access_type='offline',
             include_granted_scopes='true')
 
-        return redirect(authorization_url)
-
+        return redirect(authorization_url)  # Redirect the user to the authorization URL
 
 
 class GoogleCalendarRedirectView(APIView):
+    """
+    Handles the redirect from the Google Calendar authorization page.
+
+    This view handles the callback from the Google authorization page after the user grants
+    permissions. It fetches the access token and credentials, and uses them to retrieve the
+    list of events from the Google Calendar API.
+
+    """
     def get(self, request):
+        """
+        Handles the GET request for the Google Calendar authorization callback.
+
+        Returns:
+            Response: Returns the response with data and status.
+
+        """
         response_data = {
-            'data':None,
-            'errors':None
+            'data': None,
+            'errors': None
         }
         response_status = status.HTTP_200_OK
-        error=request.query_params.get('error', None)
+        error = request.query_params.get('error', None)  # Check if there is an error in the query parameters
 
-        if(error):
-            response_data['errors']=error
+        if error:
+            response_data['errors'] = error  # Set the error in the response data
         else:
-
-            scopes = GOOGLE_CALENDAR_SCOPES
-            redirect_uri = GOOGLE_CALENDAR_REDIRECT_URI
+            scopes = GOOGLE_CALENDAR_SCOPES  # Set the scopes for Google Calendar API
+            redirect_uri = GOOGLE_CALENDAR_REDIRECT_URI  # Set the redirect URI for OAuth2 flow
             try:
-                code = request.query_params.get('code')
-                flow=GoogleCalenderUtils.get_oauth2_flow(scopes, redirect_uri)
+                code = request.query_params.get('code')  # Get the authorization code from the query parameters
+                flow = GoogleCalenderUtils.get_oauth2_flow(scopes, redirect_uri)  # Create an OAuth2 flow
 
-                flow.fetch_token(code=code)
-                credentials=flow.credentials
-                event_list=GoogleCalenderUtils.get_event_list(credentials)
-                response_data['data']={'events':event_list,}
+                flow.fetch_token(code=code)  # Fetch the access token and credentials
+                credentials = flow.credentials  # Get the credentials for accessing Google Calendar API
+                event_list = GoogleCalenderUtils.get_event_list(credentials)  # Get the list of events using the credentials
+                response_data['data'] = {'events': event_list, }  # Set the events data in the response data
             except Exception as e:
-                response_data['errors']=str(e)
-                response_status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                response_data['errors'] = str(e)  # Set the error message in the response data
+                response_status = status.HTTP_500_INTERNAL_SERVER_ERROR  # Set the response status to internal server error
 
-        return Response(response_data, response_status,)
+        return Response(response_data, response_status,)  # Return the response with data and status
