b4ce8d77eb4e97fd19c6c4831eafd0bac631ef13
yashwant-s <72819773+yashwant-s@users.noreply.github.com>
2023-04-22 10:54:06 +0000

    removed content pf client_secrets.json file as it is a sensitive information

--- rest/urls.py
+++ rest/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import GoogleCalendarInitView, GoogleCalendarRedirectView
 
 urlpatterns = [
-    path('/v1/calendar/init/', GoogleCalendarInitView.as_view(), name = 'authorization_view'
+    path('v1/calendar/init/', GoogleCalendarInitView.as_view(), name = 'authorization_view'
 ),
     path('v1/calendar/redirect/', GoogleCalendarRedirectView.as_view(), name = 'calender_view')
 
